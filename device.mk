85acdb21298316ab7025d73f880c34a2d03fd4d8
yashraj22 <yyashraj05@gmail.com>
2020-10-02 20:14:03 +0530

    drop external camera provider

--- device.mk
+++ device.mk
@@ -180,9 +180,6 @@ PRODUCT_PACKAGES += \
     camera.msm8916 \
     Camera2
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
