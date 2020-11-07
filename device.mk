bde4b4ab8daeb832da4082c6cab598d8b7dc8cba
keneankit01 <keneankit01@gmail.com>
2020-11-07 10:31:00 +0000

    Replace media codecs from Stock

--- device.mk
+++ device.mk
@@ -302,7 +302,6 @@ PRODUCT_COPY_FILES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
